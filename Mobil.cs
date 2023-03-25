572c2a6eb1520a50b7d817b6f2ab57fb896b998e
IlhamNajii <IlhamNadzi21@students.amikom.ac.id>
2023-03-25 10:45:26 +0700

    first commit

---
+++ Mobil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    internal class Mobil
+    {
+        public string Warna { get; set; }
+        public string Merek { get; set; }
+        public string Model { get; set; }
+        public int TahunKeluaran { get; set; }
+        public int JumlahPintu { get; set; }
+        public int Kecepatan { get; set; }
+        public string Suara { get; set; }
+
+        public void Gas(int a)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}\n", Model, a);
+        }
+        public void Klakson(string a)
+        {
+            Console.WriteLine("{0}\n", Suara);
+        }
+
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1} model {2} keluaran tahun {3}" + "\ndengan jumlah pintu sebanyak {4}.", Warna, Merek, Model, TahunKeluaran, JumlahPintu);
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil = new Mobil();
+
+            mobil.Warna = "Merah";
+            mobil.JumlahPintu = 2;
+            mobil.Merek = "Toyota";
+            mobil.Model = "Supra";
+            mobil.TahunKeluaran = 1997;
+            
+            int kecepatan =  mobil.Kecepatan = 180;
+            string suara = mobil.Suara = "Tiiinn";
+
+            mobil.Gas(kecepatan);
+            mobil.Klakson(suara);
+            mobil.TampilkanInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
